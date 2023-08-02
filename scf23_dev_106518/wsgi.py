fb65d0b16a7a60617679ac5275baa640ea5a1ff0
Crowdbotics <engineering@crowdbotics.com>
2023-08-02 09:39:36 +0000

    Initial commit.

---
+++ scf23_dev_106518/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for scf23_dev_106518 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'scf23_dev_106518.settings')
+
+application = get_wsgi_application()
